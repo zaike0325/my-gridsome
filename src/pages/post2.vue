11195c9f7c7c70c1ca99f3dd56ba8d9a6380fd09
hetao <hetao@hexinyidao.com>
2021-12-27 18:37:51 +0800

    提交代码

---
+++ src/pages/post2.vue
@@ -0,0 +1,29 @@
+<template>
+  <Layout>
+      <div>
+          <h1>Post2</h1>
+      </div>
+  </Layout>
+</template>
+
+<page-query>
+    query {
+        posts: allPost {
+            edges {
+                node {
+                    id
+                    title
+                }
+            }
+        }
+    }
+</page-query>
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
